f2d325d40b840ad676a99e42beb75af8bb6fa276
myw <myw@163.com>
2024-01-03 22:55:42 +0800

    原型系统

---
+++ 交互实验文件夹/原型系统/iGestureGlove/iGestureGlove/InterAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iGestureGlove
+{
+    public partial class InterAction : Form
+    {
+        public InterAction()
+        {
+            InitializeComponent();
+        }
+    }
+}
